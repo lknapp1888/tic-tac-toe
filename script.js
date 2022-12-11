fb527d1337c1cf3c24f70f062d9f2e0c2a63d4ac
Lewis Knapp <lknapp1888@gmail.com>
2022-12-11 23:09:19 +0000

    add working prompt and result board, and reset btn

--- script.js
+++ script.js
@@ -1,50 +1,110 @@
 const GameBoard = (function() {
-    let gameBoard = new Array(9);
+    let gameBoard = ['', '', '', '', '', '', '', '', '',];
+    const resetBtn = document.getElementById('reset-btn')
 
     const addX = function(i) {
         this.gameBoard[i] = 'X';
         let square = document.querySelector(`[data-squareNum="${i}"]`);
         square.textContent = 'X'
+        this.checkWinner()
     }
     const addO = function(i) {
-            this.gameBoard[i] = 'O';
-            let square = document.querySelector(`[data-squareNum="${i}"]`);
-            square.textContent = 'O'
+        this.gameBoard[i] = 'O';
+        let square = document.querySelector(`[data-squareNum="${i}"]`);
+        square.textContent = 'O'
+        this.checkWinner()
+    }
+    const checkWinner = function() {
+        let arr = this.gameBoard;
+        for (let i = 0; i < arr.length; i += 3) {
+            if (arr[i] === undefined) continue;
+            if ((arr[i] === arr[i + 1]) && (arr[i] === arr[i + 2])) {
+              result(arr[i]);
+              return;
+            }
+          }
+          for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === undefined) continue;
+            if ((arr[i] === arr[i + 3]) && (arr[i] === arr[i + 6])) {
+                result(arr[i]);
+              return;
+            }
+          }
+          for (let i = 2; i <= 4; i+= 2) {
+            if (arr[i] === undefined) continue;
+            if ((arr[4] === arr[4 + i]) && (arr[4] === arr[4 - i])) {
+                result(arr[i]);
+              return;
+            }
+          }
+          if (!(arr.some(function(e) {return e === '';}))) {this.result('tie')}
+    }
+
+    const result = function(e) {
+        const resultBox = document.getElementById('result-container');
+        if (e === 'O') {
+            resultBox.textContent = `${playerOne.name} win!`;
+            playerOne.result = 'winner';
         }
-    return {gameBoard, addX, addO};
+        if (e === 'X') {
+            resultBox.textContent = `${playerTwo.name} win!`;
+            playerTwo.result = 'winner';
+        }
+        if (e === 'tie') {
+            playerOne.result = 'tie';
+            playerTwo.result = 'tie';
+            resultBox.textContent = 'tie!'
+        }
+    }
+    const resetGame = function() {
+        GameBoard.gameBoard = ['', '', '', '', '', '', '', '', '',];
+        let gridSquare = document.querySelectorAll('.square');
+        let announceBox = document.getElementById('announce-container');
+        let resultBox = document.getElementById('result-container');
+        gridSquare.forEach(e => e.textContent = '')
+        announceBox.textContent = `Player 1's turn (O)`;
+        playerOne.result = '';
+        playerTwo.result = '';
+        resultBox.textContent = ''
+    }
+    resetBtn.addEventListener('click', () => resetGame())
+
+    return {gameBoard, addX, addO, checkWinner, result, resetGame};
 })();
 
 const playGame = (function() {
-    const gridSquare = document.querySelectorAll('.square');
-
-
+    let gridSquare = document.querySelectorAll('.square');
+    let announceBox = document.getElementById('announce-container')
     gridSquare.forEach(e => e.addEventListener('click', e => {
         let gridNum = e.target.attributes[0].value;
             if (e.target.textContent.length > 0) return;
+            if (playerOne.result !== '' || playerTwo.result !== '') return;
             if (playerOne.active === true) {
                 GameBoard.addO(gridNum)
                 playerOne.toggleActive();
                 playerTwo.toggleActive();
+                announceBox.textContent = `Player 2's turn (X)`;
                 return;
             }
             if (playerOne.active === false) {
                 GameBoard.addX(gridNum)
                 playerOne.toggleActive();
                 playerTwo.toggleActive();
+                announceBox.textContent = `Player 1's turn (O)`;
                 return;
             }
     }))
 })();
 
-
-const player = function(active) {
+// player factory function
+const player = function(name, active) {
     toggleActive = function() {
         this.active = !this.active;
     }
-    return {active, toggleActive}
+    return {name, active, toggleActive, result: ''}
 }
 
-const playerOne = player(true);
-const playerTwo = player(false);
+const playerOne = player(`O's`, true);
+const playerTwo = player(`X's`, false);
 
 
